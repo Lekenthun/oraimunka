b5056cd7b58ece6959c5bb875bf3e852279da6e3
Diak <Diak@T018CG13TEACH>
2018-01-15 16:17:52 +0100

    új szín értem?

--- index.php
+++ index.php
@@ -7,19 +7,23 @@
         if(isset($_GET["dark"]) and !empty($_GET["dark"])) {
             if($_GET["dark"] == 1) {
                 echo '<link rel="stylesheet" href="dark.css">';
-                header('Refresh: 1; url=index.php?dark=0');
+                header('Refresh: 3; url=index.php?dark=2');
+            } elseif($_GET["dark"] == 2) {
+                echo '<link rel="stylesheet" href="dark2.css">';
+                header('Refresh: 3; url=index.php?dark=0');
             } else {
                 echo '<link rel="stylesheet" href="light.css">';
-                header('Refresh: 1; url=index.php?dark=1');
+                header('Refresh: 3; url=index.php?dark=1');
             }
         } else {
             echo '<link rel="stylesheet" href="light.css">';
-            header('Refresh: 1; url=index.php?dark=1');
+            header('Refresh: 3; url=index.php?dark=1');
         }
         ?>
     </head>
     <body>
         <a href="./index.php?dark=1">lőn sötéCSSég</a>
+        <a href="./index.php?dark=2">lőn hülyesség</a>
         <a href="./index.php?dark=0">lőn világosság</a>
     </body>
 </html>
