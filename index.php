272cc0c542b99545615df3b395c2c89828cd880f
Diak <Diak@T018CG13TEACH>
2018-01-15 16:10:31 +0100

    készeeen vaaan

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Teszt</title>
+        <?php 
+        if(isset($_GET["dark"]) and !empty($_GET["dark"])) {
+            if($_GET["dark"] == 1) {
+                echo '<link rel="stylesheet" href="dark.css">';
+                header('Refresh: 1; url=index.php?dark=0');
+            } else {
+                echo '<link rel="stylesheet" href="light.css">';
+                header('Refresh: 1; url=index.php?dark=1');
+            }
+        } else {
+            echo '<link rel="stylesheet" href="light.css">';
+            header('Refresh: 1; url=index.php?dark=1');
+        }
+        ?>
+    </head>
+    <body>
+        <a href="./index.php?dark=1">lőn sötéCCSég</a>
+        <a href="./index.php?dark=0">lőn világosság</a>
+    </body>
+</html>
