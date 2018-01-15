a49b1c6554e93818c5f330a9eaf6b23e17ed9b38
Diak <Diak@T018CG13TEACH>
2018-01-15 17:05:59 +0100

    hülyeségek kis zedve

--- index.php
+++ index.php
@@ -8,17 +8,17 @@
         if(isset($_GET["dark"]) and !empty($_GET["dark"])) {
             if($_GET["dark"] == 1) {
                 echo '<link rel="stylesheet" href="dark.css">';
-                header('Refresh: 3; url=index.php?dark=2');
+                //header('Refresh: 3; url=index.php?dark=2');
             } elseif($_GET["dark"] == 2) {
                 echo '<link rel="stylesheet" href="dark2.css">';
-                header('Refresh: 3; url=index.php?dark=0');
+                //header('Refresh: 3; url=index.php?dark=0');
             } else {
                 echo '<link rel="stylesheet" href="light.css">';
-                header('Refresh: 3; url=index.php?dark=1');
+                //header('Refresh: 3; url=index.php?dark=1');
             }
         } else {
             echo '<link rel="stylesheet" href="light.css">';
-            header('Refresh: 3; url=index.php?dark=1');
+            //header('Refresh: 3; url=index.php?dark=1');
         }
         ?>
     </head>
